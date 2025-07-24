5b3181b734359b66ad489e612c56907648aa6115
Anwita <anwita.tiwari@gmail.com>
2025-07-24 10:12:52 -0700

    FMade a new Navigation stack

--- NavigationAppKWK/ContentView.swift
+++ NavigationAppKWK/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            VStack {
+                Text("This is the Root View!")
+
+                NavigationLink(destination: Text("You've arrived at the second view!")) {
+                    Text("Click Me")
+                }
+            }
+
         }
-        .padding()
+
     }
 }
 
