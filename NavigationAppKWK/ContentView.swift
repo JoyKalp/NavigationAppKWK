ebe5a32767e24a67c054b2de5dde5c4bd1d1da7d
Anwita <anwita.tiwari@gmail.com>
2025-07-24 09:59:07 -0700

    Initial Commit

---
+++ NavigationAppKWK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationAppKWK
+//
+//  Created by Anwita Tiwari on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationAppKWK/NavigationAppKWKApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppKWKApp.swift
+//  NavigationAppKWK
+//
+//  Created by Anwita Tiwari on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAppKWKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
